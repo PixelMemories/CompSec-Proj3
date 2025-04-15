db0adc6dfd8548efae62f54bf9547fc8fb7c2380
PixelMemories <mine1122333@gmail.com>
2025-04-14 22:32:40 -0400

    finished

--- testing/hidefile.c
+++ testing/hidefile.c
@@ -1,31 +1,31 @@
-// Richard Li - rl902
-
 #define _GNU_SOURCE
-#include <string.h>
-#include <stdarg.h> // handle variable args in open
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dirent.h>
-#include <errno.h>  // for setting an error code
 #include <dlfcn.h>  // for dynamic loading
+#include <errno.h>  // for setting an error code
+#include <stdarg.h>  // handle variable args in open
 #include <fcntl.h>
 
-//Globals
+/* Typedef for the original readdir function */
 typedef struct dirent *(*orig_readdir_type)(DIR *);
 
 static orig_readdir_type real_readdir = NULL;
 
+/* Globals to store hidden file names */
 static int hidden_initialized = 0;
 static char **hidden_files = NULL;
 static int hidden_count = 0;
 
-// Function to initialize the list of hidden files 
+/* Function to initialize the list of hidden files */
 void init_hidden(void) {
     if (hidden_initialized)
         return;
 
     char *env = getenv("HIDDEN");
     if (env) {
+        /* Duplicate the string since strtok modifies it */
         char *env_copy = strdup(env);
         char *token = strtok(env_copy, ":");
         while (token != NULL) {
@@ -47,6 +47,8 @@ void init_hidden(void) {
 // This isn't the system call but is used by many programs that need to read directories, 
 // like find and ls.
 
+
+/* Replacement readdir function */
 struct dirent *readdir(DIR *dirp) {
     if (!real_readdir)
         real_readdir = (orig_readdir_type)dlsym(RTLD_NEXT, "readdir");
@@ -65,19 +67,22 @@ struct dirent *readdir(DIR *dirp) {
         }
         if (!hide)
             return entry;
+        /* Otherwise, continue looping to skip hidden entries */
     }
     return NULL;
 }
 
-// Globals 
+
+/* Typedef for the original open function */
 typedef int (*orig_open_type)(const char *pathname, int flags, ...);
 static orig_open_type real_open = NULL;
 
+/* Globals to store blocked suffixes */
 static int blocked_initialized = 0;
 static char **blocked_suffixes = NULL;
 static int blocked_count = 0;
 
-//Function to initialize the list of blocked suffixes
+/* Function to initialize the list of blocked suffixes */
 void init_blocked(void) {
     if (blocked_initialized)
         return;
@@ -107,6 +112,8 @@ void init_blocked(void) {
 //	int open(const char *pathname, int flags)
 //	int open(const char *pathname, int flags, mode_t mode)
 //
+
+/* Replacement open function */
 int open(const char *pathname, int flags, ...) {
     if (!real_open)
         real_open = (orig_open_type)dlsym(RTLD_NEXT, "open");
@@ -114,6 +121,7 @@ int open(const char *pathname, int flags, ...) {
     if (!blocked_initialized)
         init_blocked();
 
+    /* Check the pathname for any blocked suffix */
     for (int i = 0; i < blocked_count; i++) {
         size_t path_len = strlen(pathname);
         size_t suffix_len = strlen(blocked_suffixes[i]);
@@ -125,6 +133,7 @@ int open(const char *pathname, int flags, ...) {
         }
     }
 
+    /* Forward the call using variable arguments */
     int fd;
     va_list args;
     va_start(args, flags);
